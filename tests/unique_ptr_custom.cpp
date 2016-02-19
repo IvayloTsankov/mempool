7659b792f0cbaccea9004752b21ce7e1447d3a4a
IvayloTsankov <ivaylotsankov93@gmail.com>
2016-02-19 16:31:22 +0200

    * Add test for deleter with std::bind

--- tests/unique_ptr_custom.cpp
+++ tests/unique_ptr_custom.cpp
@@ -7,12 +7,25 @@ struct Vector
     float x, y, z;
 };
 
+struct Del
+{
+    void del(Vector*)
+    {
+        printf("Call Del::del\n");
+    }
+};
+
 int main()
 {
     {
         std::unique_ptr<Vector, std::function<void(Vector*)>> v(new Vector{ 1, 1, 1 },
             [](Vector* p) { printf("custom deleter called\n"); });
     }
+
+    {
+        Del d;
+        std::unique_ptr<Vector, std::function<void(Vector*)>> v(new Vector{ 2, 2, 2 },
+            std::bind(&Del::del, d, std::placeholders::_1));
+    }
     return 0;
 }
-
